1e3ea4d7c37a7fd5c01b4dffb77366b6193dfa69
petar111 <jpetar765@gmail.com>
2017-03-14 15:35:57 +0100

    Dodate hash AND eq u knjigu i autora

--- Biblioteka2017/src/biblioteka/Knjiga.java
+++ Biblioteka2017/src/biblioteka/Knjiga.java
@@ -29,6 +29,31 @@ public class Knjiga {
 	public void setIzdavac(String izdavac) {
 		this.izdavac = izdavac;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (isbn ^ (isbn >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		if (isbn != other.isbn)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", izdavac=" + izdavac + ", izdanje=" + izdanje
+				+ ", autori=" + autori + "]";
+	}
 	public int getIzdanje() {
 		return izdanje;
 	}
