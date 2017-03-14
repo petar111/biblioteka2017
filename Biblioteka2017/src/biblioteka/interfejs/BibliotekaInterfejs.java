7edd07de289f0b509e86283672c708efca3c0c17
petar111 <jpetar765@gmail.com>
2017-03-14 15:32:58 +0100

    Dodat interfejs Bb

--- Biblioteka2017/src/biblioteka/interfejs/BibliotekaInterfejs.java
+++ Biblioteka2017/src/biblioteka/interfejs/BibliotekaInterfejs.java
@@ -1,5 +1,15 @@
 package biblioteka.interfejs;
 
+import java.util.LinkedList;
+
+import biblioteka.Autor;
+import biblioteka.Knjiga;
+
 public interface BibliotekaInterfejs {
+	
+	public void dodajKnjigu(Knjiga k);
+	public void obrisiKnjigu(Knjiga k);
+	public LinkedList<Knjiga> vratiSveKnjige();
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor a, long isbn, String naslov, String izdavac);
 
 }
