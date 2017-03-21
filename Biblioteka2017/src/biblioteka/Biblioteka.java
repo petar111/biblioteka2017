74a85aa741ee27828dd8091aa0c49e495ce3e613
petar111 <jpetar765@gmail.com>
2017-03-21 14:51:29 +0100

    Uvedena logicka kontrola promena u klasu Biblioteka

--- Biblioteka2017/src/biblioteka/Biblioteka.java
+++ Biblioteka2017/src/biblioteka/Biblioteka.java
@@ -27,6 +27,10 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor a, long isbn, String naslov, String izdavac) {
+		if(a == null || isbn == 0 || naslov == null || izdavac == null){
+			return null;
+		}
+		
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
 		for(int i = 0; i < knjige.size(); i++){
 			if(knjige.get(i).getNaslov().contains(naslov)){
