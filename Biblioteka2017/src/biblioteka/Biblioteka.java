e3fea280fd9cf4261bc6afa458e6d5740bf00ce2
petar111 <jpetar765@gmail.com>
2017-03-21 14:38:10 +0100

    Uvedena klasa Biblioteka koja implementira interfejs BibliotekaInterfejs

---
+++ Biblioteka2017/src/biblioteka/Biblioteka.java
@@ -0,0 +1,39 @@
+package biblioteka;
+
+import java.util.LinkedList;
+
+import biblioteka.interfejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs {
+	
+	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
+	
+
+	@Override
+	public void dodajKnjigu(Knjiga k) {
+		knjige.add(k);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga k) {
+		knjige.remove(k);
+
+	}
+
+	@Override
+	public LinkedList<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor a, long isbn, String naslov, String izdavac) {
+		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
+		for(int i = 0; i < knjige.size(); i++){
+			if(knjige.get(i).getNaslov().contains(naslov)){
+				rezultat.add(knjige.get(i));
+			}
+		}
+		return rezultat;
+	}
+
+}
